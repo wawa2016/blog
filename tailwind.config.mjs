71b22b54d8a5def07bd50b4fac5afc8b28029243
wawa2016 <wkidziak@gmail.com>
2024-08-02 17:00:11 +0000

    chore: Add Astro configuration and dependencies

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+
+		extend: {},
+		
+		fontFamily: {
+			sans: ['Graphik', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		  },
+		
+	},
+	plugins: [],
+}
